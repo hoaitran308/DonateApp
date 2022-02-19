7218928a00129be73fee804b801e0228f9623ce0
hoaitran308 <tranvanhoai.tt4@gmail.com>
2022-02-19 23:08:38 +0700

    Add project files.

---
+++ DonateApp/Pages/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace DonateApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private IMemoryCache cache;
+        public int moneyInSession, moneyInCache;
+
+        public IndexModel(ILogger<IndexModel> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            cache = memoryCache;
+        }
+
+        public void OnGet()
+        {
+            GetMoney();
+        }
+
+        public void OnPost()
+        {
+            GetMoney();
+            SetMoney();
+            GetMoney();
+        }
+
+        public void SetMoney()
+        {
+            HttpContext.Session.SetInt32("Donation", ++moneyInSession);
+            cache.Set("Donation", ++moneyInCache);
+        }
+
+        public void GetMoney()
+        {
+            int? totalDonationInSession = HttpContext.Session.GetInt32("Donation");
+            moneyInSession = totalDonationInSession ?? 0;
+
+            cache.TryGetValue("Donation", out moneyInCache);
+        }
+    }
+}
